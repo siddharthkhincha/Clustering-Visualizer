4bfd01469baeddf813427d03cc2cfc1c184e40b5
Prakhar Pandey <3.14prakhar@gmail.com>
2023-05-02 01:15:21 +0530

    gui enhanced

--- ui.py
+++ ui.py
@@ -2,7 +2,8 @@
 import customtkinter
 from tkinter import *
 from tkinter import filedialog
-from PIL import ImageTk, Image
+import tkinter as tk
+from PIL import ImageTk, Image, ImageOps
 import os
 
 
@@ -10,6 +11,8 @@ import os
 from algorithms.affinity_propogation import call_affinity
 from algorithms.db_scan import call_dbscan
 from algorithms.k_means import call_kmeans
+
+from random_generator import random_data
 
 
 # Import file_readers
@@ -54,46 +57,52 @@ def display_ui():
         return filename
 
     def submit():
-        if (filename == ""):
-            print("file not uploaded")
-        print(filename)
-
-        temp_arr = filename.split(".")
-
-        if temp_arr[1] not in file_types:
-            labelNum4 = customtkinter.CTkLabel(
-                root, text="Upload appropriate file")
-            labelNum3.place(relx=0.3, rely=0.68)
-            labelNum4.place(relx=0.4, rely=0.58)
-            submit_button.place(relx=0.35, rely=0.8)
-            return
-        ret_arr.append(filename)
-        ret_arr.append(temp_arr[1])
-
-        # Extract file extension
-        extension = temp_arr[1]
-
-        # call appropriate file reader
+        
         global input_data
-        print(extension)
+        
+        if file_radio_choice.get() == "select_file":
+            if (filename == ""):
+                print("file not uploaded")
+            print(filename)
 
-        if extension == "csv":
-            print(type(has_header))
-            print(has_header)
-            print(type(plot_dimension_choice))
-            print(plot_dimension_choice)
-            input_data = read_csv(filename, has_header,
-                                  True, plot_dimension_choice)
-        elif extension == "xlsx":
-            input_data = read_xlsx(filename, has_header,
-                                   True, plot_dimension_choice)
-        elif extension == "xls":
-            input_data = read_xls(filename, has_header,
-                                  True, plot_dimension_choice)
-        elif extension == "mat":
-            print("HERE")
-            input_data = read_mat(filename, has_header,
-                                  True, plot_dimension_choice)
+            temp_arr = filename.split(".")
+
+            if temp_arr[1] not in file_types:
+                labelNum4 = customtkinter.CTkLabel(
+                    root, text="Upload appropriate file")
+                labelNum3.place(relx=0.3, rely=0.68)
+                labelNum4.place(relx=0.4, rely=0.58)
+                submit_button.place(relx=0.35, rely=0.8)
+                return
+            ret_arr.append(filename)
+            ret_arr.append(temp_arr[1])
+
+            # Extract file extension
+            extension = temp_arr[1]
+
+            # call appropriate file reader
+            print(extension)
+
+            if extension == "csv":
+                print(type(has_header))
+                print(has_header)
+                print(type(plot_dimension_choice))
+                print(plot_dimension_choice)
+                input_data = read_csv(filename, has_header,
+                                    True, plot_dimension_choice)
+            elif extension == "xlsx":
+                input_data = read_xlsx(filename, has_header,
+                                    True, plot_dimension_choice)
+            elif extension == "xls":
+                input_data = read_xls(filename, has_header,
+                                    True, plot_dimension_choice)
+            elif extension == "mat":
+                print("HERE")
+                input_data = read_mat(filename, has_header,
+                                    True, plot_dimension_choice)
+        else:
+            input_data = random_data_generated
+            
         if (algo_clicked.get() == "affinity clustering"):
             print(algo_clicked.get())
             ret_arr.append(algo_clicked.get())
@@ -233,25 +242,65 @@ def display_ui():
         print(num)
         if num == 0:
             customtkinter.set_appearance_mode("light")
+            theme_button.configure(image=sun_icon)
+            theme_button.image = sun_icon
             num = 1
         else:
             customtkinter.set_appearance_mode("dark")
+            theme_button.configure(image=moon_icon)
+            theme_button.image = moon_icon
             num = 0
         print(num)
 
+    def generate_random_dataset():
+        global random_data_generated
+        num_points = int(num_points_input.get())
+        num_clusters = int(num_clusters_input.get())
+        random_data_generated = random_data(num_points, num_clusters, plot_dimension_choice)
+        print(f"Generating random dataset with {num_points} points and {num_clusters} clusters.")
+        submit()
+
+    def select_file_options():
+        num_points_label.place_forget()
+        num_points_input.place_forget()
+        num_clusters_input.place_forget()
+        num_clusters_label.place_forget()  
+        generate_button.place_forget() 
+        browse_button.place(relx=0.5, rely=0.55)
+        labelNum3.place(relx=0.5, rely=0.65)
+        submit_button.place(relx=0.5, rely=0.8) 
+
+    def create_random_options():
+        num_points_label.place(relx=0.15, rely=0.55)
+        num_points_input.place(relx=0.5, rely=0.55)
+        num_clusters_input.place(relx=0.5, rely=0.65)
+        num_clusters_label.place(relx=0.15, rely=0.65)
+        generate_button.place(relx=0.5, rely=0.8)
+        browse_button.place_forget()
+        labelNum3.place_forget()
+        submit_button.place_forget()
+        
+        
+    def file_or_random_creator():
+        if file_radio_choice.get() == "select_file":
+            select_file_options()
+        else:
+            create_random_options()
 
     ############################ BUILDING UI ###################################################################
 
     file_types = ["csv", "xlsx", "xls", "mat"]
 
     customtkinter.set_appearance_mode("dark")
-    global has_header, plot_dimension_choice
-    global ret_arr
+    global has_header, plot_dimension_choice, num_clusters_entry, num_points_entry, file_radio_choice, ret_arr, num
+    num = 0 # means currently dark mode is on
     ret_arr = []
 
     global root
     root = customtkinter.CTk()
     root.geometry('500x300+100+200')
+    # use full screen
+    root.attributes('-fullscreen', True)
     customtkinter.set_appearance_mode("dark")
     root.title('PLL')
 
@@ -271,10 +320,10 @@ def display_ui():
     DimensionLabel = customtkinter.CTkLabel(root, text="Choose plot dimension")
     DimensionLabel.place(relx=0.15, rely=0.2)
 
-    dimensions = customtkinter.StringVar(value="3-D")
+    dimensions = customtkinter.StringVar(value="2-D")
 
     drop3 = customtkinter.CTkOptionMenu(master=root,
-                                        values=["1-D", "2-D", "3-D"],
+                                        values=["2-D", "3-D"],
                                         variable=dimensions)
     drop3.place(relx=0.5, rely=0.2)
 
@@ -293,30 +342,65 @@ def display_ui():
     else:
         has_header = False
     plot_dimension_choice = int(dimensions.get()[:1])
-
+    
+    
     # browse files button
     labelNum2 = customtkinter.CTkLabel(root, text="Input Data File:")
-    labelNum2.place(relx=0.15, rely=0.4)
+    labelNum2.place(relx=0.15, rely=0.45)
+
+    file_radio_choice = customtkinter.StringVar(value="select_file")
+
+
+
+    file_radio_select = customtkinter.CTkRadioButton(root, text="Select a File", variable=file_radio_choice, value="select_file", command=file_or_random_creator)
+    file_radio_select.place(relx=0.5, rely=0.45)
+
+    file_radio_create = customtkinter.CTkRadioButton(root, text="Create Random Dataset", variable=file_radio_choice, value="create_random", command=file_or_random_creator)
+    file_radio_create.place(relx=0.7, rely=0.45)
+
     browse_button = customtkinter.CTkButton(
         root, text="Browse Files", command=browseFiles)
 
-    browse_button.place(relx=0.5, rely=0.4)
     labelNum3 = customtkinter.CTkLabel(
         root, text="Only .csv, .xlsx, .xls files allowed")
-    labelNum3.place(relx=0.3, rely=0.6)
-    # submit button
+    
+
+    num_points_label = customtkinter.CTkLabel(root, text="Number of points:")
+    num_points_input = customtkinter.CTkEntry(root)
+    num_clusters_label = customtkinter.CTkLabel(root, text="Number of clusters:")
+    num_clusters_input = customtkinter.CTkEntry(root)
+
+
+    generate_button = customtkinter.CTkButton(
+        root, text="Generate", command=generate_random_dataset)
 
     submit_button = customtkinter.CTkButton(
         root, text="Submit", command=submit)
-    submit_button.place(relx=0.25, rely=0.75)
-    global num
-    num = 0
 
-    submit_button4 = customtkinter.CTkButton(
-        root, text="Change Theme", command=change_theme)
-    submit_button4.place(relx=0.62, rely=0.85)
+    file_or_random_creator()
+
+    # Adding the theme button    
+    sun_icon = PhotoImage(file="sun.png")
+    moon_icon = PhotoImage(file="moon.png")
+    # moon_icon = ImageOps.invert(moon_icon_black)
+    
+    # resize the image
+    sun_icon = sun_icon.subsample(50, 50)
+    moon_icon = moon_icon.subsample(50, 50)
+        
+    theme_button = customtkinter.CTkButton(root, image=moon_icon, command=change_theme, bg_color="transparent", border_width=0, text="" , fg_color="white" )
+    theme_button.image = moon_icon
+    theme_button.place(relx=0.9, rely=0.05, relwidth=0.05, relheight=0.05)
+    
+    
+    #Adding the close button
+    close_button = customtkinter.CTkButton(root, text="Close", command=root.destroy)
+    close_button.place(relx=0.9, rely=0.9, relwidth=0.05, relheight=0.05)
+    
+    
 
     root.mainloop()
+
 
 ##############################################################################################################
 ############################ OUTPUT VIEWER UI ################################################################
