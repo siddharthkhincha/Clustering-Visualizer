57eef0ad9c442d5e4dfcd206a19be909cd3de25b
viraj0704 <89099042+viraj0704@users.noreply.github.com>
2023-05-01 23:32:37 +0530

    Add files via upload
    Random generator file

---
+++ random_generator.py
@@ -0,0 +1,10 @@
+from sklearn import datasets
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+
+def random_data(num_samples,num_clusters,num_features):
+    X, y = make_blobs(n_samples= num_samples, centers=num_clusters, n_features=num_features,random_state=0)
+#     plt.scatter(X[:, 0], X[:, 1], marker="o", c=y, s=25, edgecolor="k")
+#     plt.show()
+    return X
