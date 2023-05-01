6a08bfffa864b9a916920085ba68789b06d3f8ab
Prakhar Pandey <3.14prakhar@gmail.com>
2023-05-02 03:17:01 +0530

    Everything working until here

--- ui.py
+++ ui.py
@@ -2,8 +2,7 @@
 import customtkinter
 from tkinter import *
 from tkinter import filedialog
-import tkinter as tk
-from PIL import ImageTk, Image, ImageOps
+from PIL import ImageTk, Image
 import os
 
 
@@ -36,7 +35,6 @@ def display_ui():
     ############################ HELPER FUNCTIONS ################################################################
 
     def browseFiles():
-        global filename
         filename = filedialog.askopenfilename(
             # initialdir=os.path.expanduser("~"),  # Start at user home directory
             initialdir=os.getcwd(),  # Start at current directory
@@ -59,25 +57,20 @@ def display_ui():
     def submit():
         
         global input_data
-        
+        plot_dimension_choice = int(dimensions.get()[:1])
         if file_radio_choice.get() == "select_file":
             if (filename == ""):
-                print("file not uploaded")
+                print("Error: File not uploaded. Try again")
+                # add a label to show that file is not uploaded, print in red
+                error_label.place(relx=0.30, rely=0.85)
+                return
+            else:
+                error_label.place_forget()
+                
             print(filename)
 
-            temp_arr = filename.split(".")
-
-            if temp_arr[1] not in file_types:
-                labelNum4 = customtkinter.CTkLabel(
-                    root, text="Upload appropriate file")
-                labelNum3.place(relx=0.3, rely=0.68)
-                labelNum4.place(relx=0.4, rely=0.58)
-                submit_button.place(relx=0.35, rely=0.8)
-                return
-            ret_arr.append(filename)
-            ret_arr.append(temp_arr[1])
-
             # Extract file extension
+            temp_arr = filename.split(".")
             extension = temp_arr[1]
 
             # call appropriate file reader
@@ -102,129 +95,6 @@ def display_ui():
                                     True, plot_dimension_choice)
         else:
             input_data = random_data_generated
-            
-        if (algo_clicked.get() == "affinity clustering"):
-            print(algo_clicked.get())
-            ret_arr.append(algo_clicked.get())
-            root.destroy()  # destroy the window
-            affinity_clustering_window()
-        elif (algo_clicked.get() == "kmeans"):
-            print(algo_clicked.get())
-            ret_arr.append(algo_clicked.get())
-            root.destroy()
-            kmeans_window()
-        elif (algo_clicked.get() == "dbscan"):
-            ret_arr.append(algo_clicked.get())
-            print(algo_clicked.get())
-            root.destroy()
-            dbscan_window()
-
-    def affinity_clustering_window():
-
-        global top
-        top = customtkinter.CTk()
-
-        top.geometry("400x250")
-        top.title('Affinity Clustering')
-
-        Parameter1 = customtkinter.CTkLabel(top, text="Damping Factor")
-        Parameter1.place(relx=0.2, rely=0.3)
-
-        Parameter2 = customtkinter.CTkLabel(top, text="Max Iterations")
-        Parameter2.place(relx=0.2, rely=0.4)
-
-        global entry1, entry2
-
-        entry1 = customtkinter.CTkEntry(master=top,
-                                        placeholder_text="> 0 , < 1",
-                                        width=120,
-                                        height=25,
-                                        border_width=2,
-                                        corner_radius=10)
-        entry1.place(relx=0.5, rely=0.3)
-        entry2 = customtkinter.CTkEntry(master=top,
-                                        placeholder_text="> 0 , < 1000",
-                                        width=120,
-                                        height=25,
-                                        border_width=2,
-                                        corner_radius=10)
-        entry2.place(relx=0.5, rely=0.4)
-
-        submit_buttonx = customtkinter.CTkButton(
-            top, text="Submit", command=submit2).place(relx=0.35, rely=0.75)
-
-        top.mainloop()
-
-    def kmeans_window():
-        global top
-        top = customtkinter.CTk()
-
-        top.geometry("400x250")
-        top.title('K-Means')
-
-        Parameter1 = customtkinter.CTkLabel(top, text="Number of Clusters")
-        Parameter1.place(relx=0.2, rely=0.3)
-
-        Parameter2 = customtkinter.CTkLabel(top, text="Max Iterations")
-        Parameter2.place(relx=0.2, rely=0.4)
-
-        global entry1, entry2
-
-        entry1 = customtkinter.CTkEntry(master=top,
-                                        placeholder_text=">1, < num_rows",
-                                        width=120,
-                                        height=25,
-                                        border_width=2,
-                                        corner_radius=10)
-        entry1.place(relx=0.5, rely=0.3)
-        entry2 = customtkinter.CTkEntry(master=top,
-                                        placeholder_text="> 0 , < 1000",
-                                        width=120,
-                                        height=25,
-                                        border_width=2,
-                                        corner_radius=10)
-        entry2.place(relx=0.5, rely=0.4)
-
-        submit_buttonx = customtkinter.CTkButton(
-            top, text="Submit", command=submit2).place(relx=0.35, rely=0.75)
-        top.mainloop()
-
-    def dbscan_window():
-        global top
-        top = customtkinter.CTk()
-
-        top.geometry("400x250")
-        top.title('DB Scan')
-
-        Parameter1 = customtkinter.CTkLabel(top, text="Epsilon")
-        Parameter1.place(relx=0.2, rely=0.3)
-
-        Parameter2 = customtkinter.CTkLabel(top, text="Min Points")
-        Parameter2.place(relx=0.2, rely=0.4)
-
-        global entry1, entry2
-
-        # e1 = customtkinter.CTkEntry(top).place(relx = 0.5,y = 0.1)
-        entry1 = customtkinter.CTkEntry(master=top,
-                                        placeholder_text="> 0 , < 1",
-                                        width=120,
-                                        height=25,
-                                        border_width=2,
-                                        corner_radius=10)
-        entry1.place(relx=0.5, rely=0.3)
-        entry2 = customtkinter.CTkEntry(master=top,
-                                        placeholder_text="> 0 , < 1000",
-                                        width=120,
-                                        height=25,
-                                        border_width=2,
-                                        corner_radius=10)
-        entry2.place(relx=0.5, rely=0.4)
-
-        submit_buttonx = customtkinter.CTkButton(
-            top, text="Submit", command=submit2).place(relx=0.35, rely=0.75)
-        top.mainloop()
-
-    def submit2():
 
         # call the appropriate algorithms
         if algo_clicked.get() == "affinity clustering":
@@ -234,7 +104,7 @@ def display_ui():
         elif algo_clicked.get() == "kmeans":
             call_kmeans(input_data, int(entry1.get()), int(entry2.get()))
 
-        top.destroy()
+        root.destroy()
         after_ui(algo_clicked.get())
 
     def change_theme():
@@ -256,26 +126,27 @@ def display_ui():
         global random_data_generated
         num_points = int(num_points_input.get())
         num_clusters = int(num_clusters_input.get())
+        plot_dimension_choice = int(dimensions.get()[:1])
         random_data_generated = random_data(num_points, num_clusters, plot_dimension_choice)
-        print(f"Generating random dataset with {num_points} points and {num_clusters} clusters.")
+        print(f"Generating random dataset with {num_points} points of {plot_dimension_choice} dimensions and {num_clusters} clusters.")
         submit()
 
-    def select_file_options():
+    def select_file_handler():
         num_points_label.place_forget()
         num_points_input.place_forget()
         num_clusters_input.place_forget()
         num_clusters_label.place_forget()  
         generate_button.place_forget() 
-        browse_button.place(relx=0.5, rely=0.55)
-        labelNum3.place(relx=0.5, rely=0.65)
-        submit_button.place(relx=0.5, rely=0.8) 
+        browse_button.place(relx=0.35, rely=0.4)
+        labelNum3.place(relx=0.33, rely=0.45)
+        submit_button.place(relx=0.35, rely=0.8) 
 
-    def create_random_options():
-        num_points_label.place(relx=0.15, rely=0.55)
-        num_points_input.place(relx=0.5, rely=0.55)
-        num_clusters_input.place(relx=0.5, rely=0.65)
-        num_clusters_label.place(relx=0.15, rely=0.65)
-        generate_button.place(relx=0.5, rely=0.8)
+    def create_random_handler():
+        num_points_label.place(relx=0.05, rely=0.4)
+        num_points_input.place(relx=0.35, rely=0.4)
+        num_clusters_input.place(relx=0.35, rely=0.45)
+        num_clusters_label.place(relx=0.05, rely=0.45)
+        generate_button.place(relx=0.35, rely=0.8)
         browse_button.place_forget()
         labelNum3.place_forget()
         submit_button.place_forget()
@@ -283,14 +154,34 @@ def display_ui():
         
     def file_or_random_creator():
         if file_radio_choice.get() == "select_file":
-            select_file_options()
+            select_file_handler()
         else:
-            create_random_options()
-
+            create_random_handler()
+            
+    def update_parameters(something):
+        if algo_clicked.get() == "affinity clustering":
+            Parameter1.configure(text="Damping")
+            Parameter2.configure(text="Max Iterations")
+            entry1.configure(placeholder_text="> 0 , < 1")
+            entry2.configure(placeholder_text="> 0 , < 1000")
+        elif algo_clicked.get() == "dbscan":
+            Parameter1.configure(text="Epsilon")
+            Parameter2.configure(text="Min Points")
+            entry1.configure(placeholder_text="Example: 3")
+            entry2.configure(placeholder_text="> 0 , < 1000")
+        elif algo_clicked.get() == "kmeans":
+            Parameter1.configure(text="Number of Clusters")
+            Parameter2.configure(text="Max Iterations")
+            entry1.configure(placeholder_text=">0 , < num_points")
+            entry2.configure(placeholder_text="> 0 , < 1000")
+            
     ############################ BUILDING UI ###################################################################
 
     file_types = ["csv", "xlsx", "xls", "mat"]
-
+    
+    global filename
+    filename = ""
+    
     customtkinter.set_appearance_mode("dark")
     global has_header, plot_dimension_choice, num_clusters_entry, num_points_entry, file_radio_choice, ret_arr, num
     num = 0 # means currently dark mode is on
@@ -307,56 +198,58 @@ def display_ui():
     # algorithm type dropdown
 
     labelNum1 = customtkinter.CTkLabel(root, text="Choose algorithm")
-    labelNum1.place(relx=0.15, rely=0.1)
+    labelNum1.place(relx=0.05, rely=0.1)
 
     algo_clicked = customtkinter.StringVar(value="affinity clustering")
 
     drop2 = customtkinter.CTkOptionMenu(master=root,
                                         values=["affinity clustering",
                                                 "dbscan", "kmeans"],
-                                        variable=algo_clicked)
-    drop2.place(relx=0.5, rely=0.1)
+                                        variable=algo_clicked, command=update_parameters)
+    drop2.place(relx=0.35, rely=0.1)
 
     DimensionLabel = customtkinter.CTkLabel(root, text="Choose plot dimension")
-    DimensionLabel.place(relx=0.15, rely=0.2)
+    DimensionLabel.place(relx=0.05, rely=0.15)
 
-    dimensions = customtkinter.StringVar(value="2-D")
+    dimensions = customtkinter.StringVar(value="3-D")
 
     drop3 = customtkinter.CTkOptionMenu(master=root,
                                         values=["2-D", "3-D"],
                                         variable=dimensions)
-    drop3.place(relx=0.5, rely=0.2)
+    drop3.place(relx=0.35, rely=0.15)
 
     HeaderLabel = customtkinter.CTkLabel(root, text="Data Headers:")
-    HeaderLabel.place(relx=0.15, rely=0.3)
+    HeaderLabel.place(relx=0.05, rely=0.2)
 
     header = customtkinter.StringVar(value="No")
 
     drop4 = customtkinter.CTkOptionMenu(master=root,
                                         values=["No", "Yes"],
                                         variable=header)
-    drop4.place(relx=0.5, rely=0.3)
+    drop4.place(relx=0.35, rely=0.2)
 
     if (header.get() == "True"):
         has_header = True
     else:
         has_header = False
+    # convert string to int
+
     plot_dimension_choice = int(dimensions.get()[:1])
     
     
     # browse files button
     labelNum2 = customtkinter.CTkLabel(root, text="Input Data File:")
-    labelNum2.place(relx=0.15, rely=0.45)
+    labelNum2.place(relx=0.05, rely=0.3)
 
     file_radio_choice = customtkinter.StringVar(value="select_file")
 
 
 
     file_radio_select = customtkinter.CTkRadioButton(root, text="Select a File", variable=file_radio_choice, value="select_file", command=file_or_random_creator)
-    file_radio_select.place(relx=0.5, rely=0.45)
+    file_radio_select.place(relx=0.20, rely=0.3)
 
     file_radio_create = customtkinter.CTkRadioButton(root, text="Create Random Dataset", variable=file_radio_choice, value="create_random", command=file_or_random_creator)
-    file_radio_create.place(relx=0.7, rely=0.45)
+    file_radio_create.place(relx=0.35, rely=0.3)
 
     browse_button = customtkinter.CTkButton(
         root, text="Browse Files", command=browseFiles)
@@ -376,8 +269,39 @@ def display_ui():
 
     submit_button = customtkinter.CTkButton(
         root, text="Submit", command=submit)
+    
+    error_label = customtkinter.CTkLabel( root, text="ERROR: File not uploaded. Try again", text_color="red" )
+    error_label.place_forget()
 
     file_or_random_creator()
+    
+    # add a label parameters
+    parameters_label = customtkinter.CTkLabel(root, text="Parameters:")
+    parameters_label.place(relx=0.05, rely=0.6)
+    
+    
+    Parameter1 = customtkinter.CTkLabel(root, text="Damping Factor:")
+    Parameter1.place(relx=0.05, rely=0.65)
+
+    Parameter2 = customtkinter.CTkLabel(root, text="Max Iterations:")
+    Parameter2.place(relx=0.05, rely=0.7)
+
+    entry1 = customtkinter.CTkEntry(master=root,
+                                    placeholder_text="> 0 , < 1",
+                                    width=120,
+                                    height=25,
+                                    border_width=2,
+                                    corner_radius=10)
+    
+    entry1.place(relx=0.35, rely=0.65)
+    entry2 = customtkinter.CTkEntry(master=root,
+                                    placeholder_text="> 0 , < 1000",
+                                    width=120,
+                                    height=25,
+                                    border_width=2,
+                                    corner_radius=10)
+    entry2.place(relx=0.35, rely=0.7)
+
 
     # Adding the theme button    
     sun_icon = PhotoImage(file="sun.png")
@@ -454,6 +378,9 @@ def slideshow(algorithm):
         # update slider value
         slider.set(img_no)
 
+    def close_button_handler():
+        slideshow_root.destroy()
+        display_ui()
 
     ############################ BUILDING UI ###################################################################
 
@@ -498,7 +425,7 @@ def slideshow(algorithm):
     slider.place(relx=0.3, rely=0.85, relwidth=0.4)
 
     close_button = customtkinter.CTkButton(
-        slideshow_root, text="Close", command=slideshow_root.destroy)
+        slideshow_root, text="Close", command=close_button_handler)
 
     close_button.place(relx=0.9, rely=0.05)
 
@@ -515,18 +442,21 @@ def after_ui(algorithm):
     def show_slideshow(algorithm):
         root2.destroy()
         slideshow(algorithm)
+        
+    def home_handler():
+        root2.destroy()
+        display_ui()
 
     ############################ BUILDING UI ###################################################################
 
     global root2
     root2 = customtkinter.CTk()
     root2.geometry('400x200+100+200')
-
+    root2.attributes('-fullscreen', True)
     root2.title('See results')
-
-    slideshow_button = customtkinter.CTkButton(
-        root2, text="See slideshow", command=show_slideshow(algorithm)).place(relx=0.35, rely=0.2)
-
+    customtkinter.CTkButton( root2, text="See slideshow", command=show_slideshow(algorithm)).place(relx=0.35, rely=0.15)
+    customtkinter.CTkButton( root2, text="Home", command=home_handler).place(relx=0.35, rely=0.35)
+    customtkinter.CTkButton( root2, text="Close", command=root2.destroy).place(relx=0.35, rely=0.55)
     root2.mainloop()
 
 
